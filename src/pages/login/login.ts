cfc272b40e8ddf26a17caf0c2d7e2bdd7352dea2
zn <zn993160060@icloud.com>
2018-12-05 16:57:47 +0800

    增加注册功能zn

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ForgetPage } from '../forget/forget';
-import { Http } from "@angular/http";
+//import { Http } from "@angular/http";
 import'rxjs/add/operator/map';
 import { HttpParams } from '@angular/common/http';
 import {HttpClient} from "@angular/common/http";
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the LoginPage page.
  *
@@ -27,12 +28,14 @@ export class LoginPage {
   yanzhengma: Number;
   zhucepassword: Number;
   errorMessage: string;
-  
+  error:any;
+  yzma:Number;
+
   isClick(i){
     this.isActive = i;
   }
 
-  constructor(private httpt:HttpClient,public http:Http, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alertCtrl: AlertController,private http:HttpClient, public navCtrl: NavController, public navParams: NavParams) {
     for (let i = 0; i < 30; i++) {
       this.items.push( this.items.length );
     }
@@ -40,27 +43,60 @@ export class LoginPage {
   goforget(){
     this.navCtrl.push(ForgetPage);
   }
-  gobpage(){
-    this.navCtrl.popToRoot();
-
+  gobpage(){                    //登录页面
     this.http.post('http://localhost:3000/customers',{
       "name":this.logintel,
       "password":this.loginpassword
     }).subscribe(res => {
       console.log(res);
-      //this.logintel = res.toString();
+      this.navCtrl.popToRoot();
     },error =>{
       console.log("Error",error)
     })
-    const params = new HttpParams().set('_page', "1").set('_limit', "1");
-    this.http.get("http://localhost:3000/customers",{params});
+    const params = new HttpParams().set('_page', "2").set('_limit', "1");
+    this.http.get("http://localhost:3000/customers",{params})
+    .subscribe(res=>{
+      console.log(res);
+    },error =>{
+      console.log("Error",error)
+    });
     
   }
-  gologin(){
-    this.navCtrl.push(LoginPage);
+  gologin(){          //注册页面
+    
     console.log("用户名："+this.zhucetel);
     console.log("验证码："+this.yanzhengma);
     console.log("密码："+this.zhucepassword);
+    var params = JSON.stringify({
+      "stu_phone":this.zhucetel,
+      "stu_password":this.zhucepassword
+      })
+    if(this.yzma == this.yanzhengma ){
+      this.http.post('http://www.zhuoran.fun:3000/register_stu',params).subscribe(res => {
+        console.log(res);
+        this.navCtrl.push(LoginPage);
+        alert("注册成功");
+      },error =>{
+        console.log("Error:",error)
+      })
+    }
+    else{
+      alert("请输入正确验证码");
+    }
+  } 
+  yanzheng(){           //验证码
+    if(this.zhucetel != null){
+      this.http.get("http://www.zhuoran.fun:3000" + "/verify?stu_phone=" + this.zhucetel ).subscribe(
+      data => {
+        console.log(data);
+        this.yzma = data.tpl_value;
+      },error=>{
+        console.log("ERROR:",error)
+      })
+    }else{
+      alert("请输入手机号");
+    }
+    
   }
   
  
